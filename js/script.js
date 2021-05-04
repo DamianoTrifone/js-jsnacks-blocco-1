27b27f22e839470e89b1a6b92e4662451658b74e
DamianoTrifone <trifone.damiano@gmail.com>
2021-05-04 13:08:51 +0200

    Collegamento js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Snack #4:
+// Generatore di “nomi cognomi” casuali: scrivere una funzione che, partendo da una lista di nomi e una lista di cognomi, generi una coppia nome-cognome casuale, così da permettere al signor Gatsby di costruire una falsa lista di 10 invitati alla sua festa, da stampare in pagina, utilizzando il tag adatto alle liste.
+// Snack #5:
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,
+// se è dispari (verifichiamolo con una funzione) inseriscilo nell’array.
+// Stampa l'array nella console.
+
+// Snack 4
